300c13aa8da30dc6e5086da4ebb832a4370dfa4e
Cannon, Bryant <Bryant.Cannon@disney.com>
2014-07-10 10:15:52 -0700

    Added store links and spots to put more game entries

---
+++ newsite/js/parallax.js
@@ -0,0 +1,77 @@
+﻿
+$(document).ready(function() {
+	$('a[href*=#]').each(function() {
+		if(location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+		&& location.hostname == this.hostname
+		&& this.hash.replace(/#/,'') ) {
+			var $targetId = $(this.hash), $targetAnchor = $('[name=' + this.hash.slice(1) + ']');
+			var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor: false;
+			if($target) {
+				var targetOffset = $target.offset().top;
+				
+				$(this).click(function() {
+					$("#nav li a").removeClass("active");
+					$(this).addClass('active');
+					$('html, body').animate({scrollTop: targetOffset}, 1000);
+					return false;
+				});
+			}
+		}
+	});
+	
+	$window = $(window);
+	$('header[id="header"]').each(function(){
+		var $bgobj = $(this); // assigning the object
+	 
+		$(window).scroll(function() {
+			var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+			
+			if(yPos < -155)
+			{
+				yPos = -155;
+			}
+			if(yPos > 0)
+			{
+				yPos = 0;
+			}
+			 
+			// Put together our final background position
+			var coords = yPos + 'px';
+ 
+			// Move the background
+			$bgobj.css({ "margin-top": coords });
+		}); 
+	}); 
+	
+	$('div[class="content_container"]').each(function(){
+		var $bgobj = $(this); // assigning the object
+	 
+		$(window).scroll(function() {
+			 var objPos = ($window.scrollTop()-($bgobj.offset().top+$bgobj.height()/2)) + $window.height()/2;
+			 
+			 
+			// Put together our final background position
+			var coords = '0px ' + (-objPos/$bgobj.data('speed') + $bgobj.data('offset-y')) + 'px';
+			var alpha = Math.max(1,$window.height()/300) - Math.abs(objPos/100);
+ 
+			// Move the background
+			$bgobj.css({ "background-position": coords });
+		//	$bgobj.css({ "opacity": alpha });
+		}); 
+		
+		/*$(window).on({'touchmove': function(e) { 
+			
+			var objPos = ($window.scrollTop()-($bgobj.offset().top+$bgobj.height()/2)) + $window.height()/2;
+			
+			 
+			// Put together our final background position
+			var coords = '0px ' + (-objPos/$bgobj.data('speed') + $bgobj.data('offset-y')) + 'px';
+			var alpha = Math.max(1,$window.height()/300) - Math.abs(objPos/100);
+ 
+			// Move the background
+			$bgobj.css({ "background-position": coords });
+			}
+		});*/
+	});
+});
+		
