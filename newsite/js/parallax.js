ad699afda60a12e468dbd0a2190fd4145e98c83c
Cannon, Bryant <Bryant.Cannon@disney.com>
2014-07-14 22:45:08 -0700

    Image fixes and resizings

--- newsite/js/parallax.js
+++ newsite/js/parallax.js
@@ -20,42 +20,48 @@ $(document).ready(function() {
 	});
 	
 	$window = $(window);
-	$('header[id="header"]').each(function(){
-		var $bgobj = $(this); // assigning the object
-	 
-		$(window).scroll(function() {
-			var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
-			
-			if(yPos < -155)
-			{
-				yPos = -155;
-			}
-			if(yPos > 0)
-			{
-				yPos = 0;
-			}
-			 
-			// Put together our final background position
-			var coords = yPos + 'px';
- 
-			// Move the background
-			$bgobj.css({ "margin-top": coords });
-		}); 
-	}); 
 	
-	$('div[title="scrolling"]').each(function(){
-		var $bgobj = $(this); // assigning the object
+	//if( !(navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPod/i)) )
+	{
+		$('header[id="header"]').each(function(){
+			var $bgobj = $(this); // assigning the object
 	 
-		$(window).scroll(function() {
-			 var objPos = ($window.scrollTop()-($bgobj.offset().top+$bgobj.height()/2)) + $window.height()/2;
+			$(window).scroll(function() {
+				var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+			
+				if(yPos < -155)
+				{
+					yPos = -155;
+				}
+				if(yPos > 0)
+				{
+					yPos = 0;
+				}
 			 
-			 
-			// Put together our final background position
-			var coords = '0px ' + (-objPos/$bgobj.data('speed') + $bgobj.data('offset-y')) + 'px';
+				// Put together our final background position
+				var coords = yPos + 'px';
  
-			// Move the background
-			$bgobj.css({ "background-position": coords });
+				// Move the background
+				$bgobj.css({ "margin-top": coords });
+			}); 
 		}); 
-	});
+	
+		$('div[title="scrolling"]').each(function(){
+			var $bgobj = $(this); // assigning the object
+	 
+			$(window).scroll(function() {
+				 var objPos = ($window.scrollTop()-($bgobj.offset().top+$bgobj.height()/2)) + $window.height()/2;
+			 
+				 var speed = -objPos/$bgobj.data('speed');
+				 var offset = $bgobj.data('offset-y');
+			 
+				// Put together our final background position
+				var coords = '0px ' + (speed + offset) + 'px';
+ 
+				// Move the background
+				$bgobj.css({ "background-position": coords });
+			}); 
+		});
+	}
 });
 		
